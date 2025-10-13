263d15d646ecf721dcacdcd53555a9e0046ec504
ngohoaithanh <thanhhuykks03@gmail.com>
2025-10-13 14:25:23 +0700

    rating completed

--- api/order/get_order_by_id.php
+++ api/order/get_order_by_id.php
@@ -15,7 +15,7 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
         SELECT 
             o.ID, o.CustomerID, o.ShipperID, o.Pick_up_address, o.Pick_up_lat, o.Pick_up_lng, 
             o.Recipient, o.RecipientPhone, o.Delivery_address, o.Delivery_lat, o.Delivery_lng, 
-            o.Status, o.COD_amount, o.Shippingfee, o.Weight, o.Created_at, o.Note, o.CODFee,  
+            o.Status, o.COD_amount, o.Shippingfee, o.Weight, o.Created_at, o.Note, o.CODFee, o.is_rated, 
             u.Username AS UserName, u.Email AS CustomerEmail, u.PhoneNumber AS PhoneNumberCus,
             u2.Username AS ShipperName, u2.Email AS ShipperEmail,
             u2.rating AS ShipperRating,
--- api/order/order_for_shipper.php
+++ api/order/order_for_shipper.php
@@ -19,11 +19,13 @@ if (!$conn) {
 // Query lấy toàn bộ user cùng role
 $sql = "SELECT o.ID, o.CustomerID, o.ShipperID, o.Pick_up_address, o.Pick_up_lat, o.Pick_up_lng, o.Recipient, o.RecipientPhone,
        o.Delivery_address, o.Delivery_lat, o.Delivery_lng, o.Status, o.COD_amount, o.Shippingfee, o.Weight, o.Created_at, o.Accepted_at, o.Note, 
+       r.rating_value,
        u.Username AS UserName, u.Email AS CustomerEmail,
        u2.Username AS ShipperName, u2.Email AS ShipperEmail, u.PhoneNumber AS PhoneNumberCus
 FROM orders o 
 LEFT JOIN users u ON o.CustomerID = u.ID 
 LEFT JOIN users u2 ON o.ShipperID = u2.ID
+LEFT JOIN ratings r ON o.ID = r.order_id
 WHERE o.ShipperID = $shipperID
 ORDER BY Created_at desc";
 
